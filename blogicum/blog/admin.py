6b82a87f9e14c89a2ad4408107c43a4896d4f96e
DenisEpishin <denepish@yandex.ru>
2024-12-25 14:46:54 +0300

    First commit

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,7 @@
+from .models import Category, Location, Post
+from django.contrib import admin
+
+
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Post)
---
+++ blogicum/blog/views.py
@@ -0,0 +1,39 @@
+from django.utils import timezone
+from .models import Post, Category
+from django.shortcuts import render, get_object_or_404
+
+
+def category_posts(request, category_slug):
+    """Отображение публикаций категории"""
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    context = {'category': category,
+               'post_list': posts().filter(category=category)}
+    return render(request, 'blog/category.html', context)
+
+
+def index(request):
+    """Главная страница / Лента записей"""
+    return render(request, 'blog/index.html', {'post_list': posts()[:5]})
+
+
+def posts():
+    """Получение постов из БД"""
+    return Post.objects.select_related(
+        'category',
+        'location',
+        'author'
+    ).filter(
+        category__is_published=True,
+        is_published=True,
+        pub_date__lte=timezone.now()
+    )
+
+
+def post_detail(request, id):
+    """Отображение полного описания выбранной записи"""
+    post = get_object_or_404(posts(), id=id)
+    return render(request, 'blog/detail.html', {'post': post})
